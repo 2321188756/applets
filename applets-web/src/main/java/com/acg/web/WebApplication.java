3d62f8f9ebb83464962c6da7cefd8f840c191e2b
shadow <2321188756@qq.com>
2021-10-26 15:54:56 +0800

    update project

--- applets-web/src/main/java/com/acg/web/WebApplication.java
+++ applets-web/src/main/java/com/acg/web/WebApplication.java
@@ -2,8 +2,13 @@ package com.acg.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScans({@ComponentScan("com.acg.auth")})
 public class WebApplication {
 
     public static void main(String[] args) {
--- applets-web/src/main/java/com/acg/web/controller/HomeController.java
+++ applets-web/src/main/java/com/acg/web/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.acg.web.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
+    @ApiOperation(value = "home page")
     @RequestMapping(value = {"/","index","index.html"})
     public String hello(){
         return "hello springboot";
