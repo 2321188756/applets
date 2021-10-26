120f2b410ec71209fa84088131d54e7c2736007c
shadow <2321188756@qq.com>
2021-10-26 16:04:00 +0800

    update project

---
+++ applets-web/src/main/java/com/acg/auth/MySecurityConfig.java
@@ -0,0 +1,29 @@
+package com.acg.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin() // 表单登录
+                .loginProcessingUrl("/login") // 处理表单登录 URL
+                .and()
+                .authorizeRequests() // 授权配置
+                .antMatchers("/authentication/require", "/login.html").permitAll() // 登录跳转 URL 无需认证
+                .anyRequest()  // 所有请求
+                .authenticated() // 都需要认证
+                .and().csrf().disable();
+    }
+}
